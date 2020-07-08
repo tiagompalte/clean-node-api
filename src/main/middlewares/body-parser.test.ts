b9902a44c624bf9eb2606423d748236eb9fe6d97
Tiago M Palte <tiagompalte@hotmail.com>
2020-07-07 21:16:44 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,10 +2,10 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  app.post('/test_body_parser', (req, res) => {
-    res.send(req.body)
-  })
   test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
     await request(app)
       .post('/test_body_parser')
       .send({ name: 'Teste' })
--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,10 +2,10 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('CORS Middleware', () => {
-  app.get('/test_cors', (req, res) => {
-    res.send()
-  })
   test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
     await request(app)
       .get('/test_cors')
       .expect('access-control-allow-origin', '*')
