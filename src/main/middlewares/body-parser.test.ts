185aa6996baa9012fef7f4080ca7352c07ec5294
Tiago M Palte <tiagompalte@hotmail.com>
2020-07-07 21:05:00 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  app.post('/test_body_parser', (req, res) => {
+    res.send(req.body)
+  })
+  test('Should parse body as json', async () => {
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Teste' })
+      .expect({ name: 'Teste' })
+  })
+})
