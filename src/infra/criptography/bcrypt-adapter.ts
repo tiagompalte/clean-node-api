a5fc547292b5ef452169e2f94cce963a7f5b2491
Tiago M Palte <tiagompalte@hotmail.com>
2022-03-02 10:27:25 -0500

    feat: implements protocols hashComparer in BcryptAdapter

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,13 +1,15 @@
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/criptography/hash-comparer'
 import { Hasher } from '../../data/protocols/criptography/hasher'
 
-export class BcryptAdapter implements Hasher {
-  private readonly salt: number
-  constructor (salt: number) {
-    this.salt = salt
-  }
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor (private readonly salt: number) {}
 
   async hash (value: string): Promise<string> {
     return bcrypt.hash(value, this.salt)
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value, hash)
   }
 }
