86dab311f1b317a0d28345c9734a0274e0bf6be0
Tiago Palte <tpalte@redventures.com>
2022-03-03 18:53:56 -0500

    refactor: remove duplicate code

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,9 @@
 import { AddAccount, AddAccountModel, Hasher, AccountModel, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  private readonly hasher: Hasher
-  private readonly addAccountRepositoryStub: AddAccountRepository
-  constructor (hasher: Hasher, addAccountRepositoryStub: AddAccountRepository) {
-    this.hasher = hasher
-    this.addAccountRepositoryStub = addAccountRepositoryStub
-  }
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly addAccountRepositoryStub: AddAccountRepository) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -3,7 +3,10 @@ import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
 import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
-  constructor (private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) {}
+  constructor (
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
